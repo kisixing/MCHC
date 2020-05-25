96a77a19724d3efd7c227560cbb66541eb1bbcf4
zwj7181 <45657181@qq.com>
2020-05-25 17:40:33 +0800

    refactor：edit form 重置、保存按钮样式调整

--- src/pages/child-management/child-death/edit/index.tsx
+++ src/pages/child-management/child-death/edit/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import Form from './components/Form';
 import { toApi, fromApi } from './config/adapter';
 import BaseEditPanel from '@/components/BaseEditPanel';
 import WithDynamicExport from '@/components/WithDynamicExport';
 
 @WithDynamicExport
-export default class WifePanel extends BaseEditPanel {
+export default class ChildDeathPanel extends BaseEditPanel {
   static defaultProps = {
     baseUrl: 'child-deaths',
     moduleName: 'child-death',
